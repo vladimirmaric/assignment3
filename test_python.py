617efe3097d9788c0e52315e186a73787a1f013c
pqrvlad <ricma96@gmail.com>
2024-12-05 12:28:08 +0100

    ispravljen kod

--- test_python.py
+++ test_python.py
@@ -1,8 +1,8 @@
 someNumber = 10
 
-if(someNumber <= 0):
-    print("Over zerro")
-elif(someNumber < 0):
-    print("Bellow zerro")
+if someNumber > 0:
+    print("Over zero")
+elif someNumber < 0:
+    print("Bellow zero")
 else:
-    print("zerro")
+    print("Zero")
